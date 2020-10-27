66b11f6968a7477e0874b60531640d45c024af8a
Rizkym13 <rizkymaulida005@gmail.com>
2020-10-27 16:24:23 +0700

    load_view

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('template/header');
+		//$this->load->view('template/js');
+		$this->load->view('template/navbar');
+		$this->load->view('template/footer');
 	}
 }
---
+++ application/models/M_login.php
@@ -0,0 +1,10 @@
+<?php
+defined ('BASEPATH') OR exit ('No direct script acess allowed');
+
+class M_login extends CI_Model{
+
+    public function login($post){
+        $this->db->get();
+    } 
+
+}
