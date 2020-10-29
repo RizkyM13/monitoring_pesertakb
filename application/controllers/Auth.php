342341d17bed2263d659f2ce8f5f76098a04fa30
Rizkym13 <rizkymaulida005@gmail.com>
2020-10-29 14:52:53 +0700

    up new2

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -1,12 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Login extends CI_Controller {
+class Auth extends CI_Controller {
 
 	public function __construct()
 	{
 		parent::__construct();
-		//$this->load->model('M_login');
+		$this->load->model('M_login');
 	}
 
 	public function login(){
