b0384b1752a0464a1be8b81a717cf592edcbae73
Rizkym13 <rizkymaulida005@gmail.com>
2020-10-29 15:00:10 +0700

    up_coba

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Auth extends CI_Controller {
+class Admin extends CI_Controller {
 
 	public function login(){
 		$this->load->view('login');
