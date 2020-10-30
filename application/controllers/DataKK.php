fb69fc35211a08f3f1df982f5c7dc16f81333f73
Waafiq29 <45926327+Waafiq29@users.noreply.github.com>
2020-10-30 13:38:47 +0700

    data kk

---
+++ application/controllers/DataKK.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class DataKK extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('template/header');
+		$this->load->view('template/navbar');
+		$this->load->view('data_kk');
+		$this->load->view('template/footer');
+	}
+}
