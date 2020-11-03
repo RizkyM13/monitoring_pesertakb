80e1409a0128da9e798627d612206b7d5560af99
berlianarohmahaminin <44019028+berlianarohmahaminin@users.noreply.github.com>
2020-11-03 14:04:21 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost:8080',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'prakerin',
