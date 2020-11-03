5aa401da1dc0d2068c65b29dfcc7ca79618e1c66
Waafiq29 <45926327+Waafiq29@users.noreply.github.com>
2020-11-03 18:24:56 +0700

    data akseptor

---
+++ application/views/transaksi/dataakseptor.php
@@ -0,0 +1,106 @@
+
+<!-- isi halaman memulai -->
+    <section id="main-content">
+      <section class="wrapper">
+        <h3><i class="fa fa-angle-right"></i>Data Akseptor</h3>
+          <div class="row mt">
+          <div class="col-md-12">
+            <div class="content-panel">
+              <table class="table table-striped table-advance table-hover">
+                <h4><i class="fa fa-angle-right"></i>Data Akseptor</h4>
+                <hr>
+                <thead>
+                  <tr>
+                    <th><i></i>ID Kontrasepsi</th>
+                    <th><i></i>Kode Kontrasepsi</th>
+                    <th><i></i>Nama Kontrasepsi</th>
+                    <th><i></i>Waktu Kadaluarsa</th>
+                    <th><i></i>Kontrasepsi Aktif</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <label class="sr-only" for=""></label>
+                      <input type="text" class="form-control" id="">
+                    </td>
+
+                    <td>
+                      <label class="sr-only" for=""></label>
+                      <input type="text" class="form-control" id="">
+                    </td>
+
+                    <td>
+                      <label class="sr-only" for=""></label>
+                      <input type="text" class="form-control" id="">
+                    </td>
+
+                    <td>
+                      <label class="sr-only" for=""></label>
+                      <input type="text" class="form-control" id="">
+                    </td>
+
+                    <td>
+                      <label class="sr-only" for=""></label>
+                      <input type="text" class="form-control" id="">
+                    </td>
+
+                    <td>
+                      <button type="submit" class="btn btn-theme">Input</button>
+                    </td>
+
+                  </tr>
+                
+                </tbody>
+              </table>
+            </div>
+            <!-- /content-panel -->
+          </div>
+          <!-- /col-md-12 -->
+        </div>
+        <!-- BASIC FORM ELELEMNTS -->
+        <div class="row mt">
+          <div class="col-md-12">
+            <div class="content-panel">
+              <table class="table table-striped table-advance table-hover">
+                <h4><i class="fa fa-angle-right"></i>Data Akseptor</h4>
+                <hr>
+                <thead>
+                  <tr>
+          
+                    <th><i></i>ID Kontrasepsi</th>
+                    <th><i></i>Kode Kontrasepsi</th>
+                    <th><i></i>Nama Kontrasepsi</th>
+                    <th><i></i>Waktu Kadaluarsa</th>
+                    <th><i></i>Kontrasepsi Aktif</th>
+                    <th>Aksi</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                    </td>
+                  </tr>
+                
+                </tbody>
+              </table>
+            </div>
+            <!-- /content-panel -->
+          </div>
+          <!-- /col-md-12 -->
+        </div>        <!-- /row -->
+
+
+        <!-- /row -->
+      </section>
+      <!-- /wrapper -->
+    </section>
+
+<!-- isi halaman end -->
