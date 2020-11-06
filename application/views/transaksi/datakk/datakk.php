e9dde33174b1775dc89a61063342e70aae39e9d9
Rizkym13 <rizkymaulida005@gmail.com>
2020-11-06 13:44:06 +0700

    upload_tampilan_part5

---
+++ application/views/transaksi/datakk/datakk.php
@@ -0,0 +1,105 @@
+<section id="main-content">
+  <section class="wrapper">
+    <div class="row mt">
+      <div class="col-lg-12">
+        <div class="form-panel">
+          <h4 class="mb">Judul File</h4>
+        </div>
+      </div>
+
+      <div class="col-md-12">
+        <div class="form-panel">
+          <a href="<?php echo base_url() . 'transaksi/datakk/tambah'; ?>" class="btn btn-primary">Primary</a>
+          <hr>
+      <table class="table table-striped table-advance table-hover">
+                
+                
+                <thead>
+                  <tr>
+                    <th><i class="fa fa-bullhorn"></i> Company</th>
+                    <th class="hidden-phone"><i class="fa fa-question-circle"></i> Descrition</th>
+                    <th><i class="fa fa-bookmark"></i> Profit</th>
+                    <th><i class=" fa fa-edit"></i> Status</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>
+                      <a href="basic_table.html#">Company Ltd</a>
+                    </td>
+                    <td class="hidden-phone">Lorem Ipsum dolor</td>
+                    <td>12000.00$ </td>
+                    <td><span class="label label-info label-mini">Due</span></td>
+                    <td>
+                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a href="basic_table.html#">
+                        Dashio co
+                        </a>
+                    </td>
+                    <td class="hidden-phone">Lorem Ipsum dolor</td>
+                    <td>17900.00$ </td>
+                    <td><span class="label label-warning label-mini">Due</span></td>
+                    <td>
+                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a href="basic_table.html#">
+                        Another Co
+                        </a>
+                    </td>
+                    <td class="hidden-phone">Lorem Ipsum dolor</td>
+                    <td>14400.00$ </td>
+                    <td><span class="label label-success label-mini">Paid</span></td>
+                    <td>
+                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a href="basic_table.html#">Dashio ext</a>
+                    </td>
+                    <td class="hidden-phone">Lorem Ipsum dolor</td>
+                    <td>22000.50$ </td>
+                    <td><span class="label label-success label-mini">Paid</span></td>
+                    <td>
+                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <a href="basic_table.html#">Total Ltd</a>
+                    </td>
+                    <td class="hidden-phone">Lorem Ipsum dolor</td>
+                    <td>12120.00$ </td>
+                    <td><span class="label label-warning label-mini">Due</span></td>
+                    <td>
+                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
+                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
+                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /content-panel -->
+        </div>
+      </div>
+    </section>
+  </section>
+
+        
