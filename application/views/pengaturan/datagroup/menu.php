c1dfcde7e34ffbcbbebf7606e3446214febbcf09
Rizkym13 <rizkymaulida005@gmail.com>
2020-12-29 16:48:18 +0700

    update_tampilan_setting_user_group

--- application/views/pengaturan/datagroup/menu.php
+++ application/views/pengaturan/datagroup/menu.php
@@ -18,35 +18,116 @@
                 <h5><i class="fa fa-tasks"></i> Administrator</h5>
                 <table id="todo" class="table table-hover custom-check">
                   <tbody>
+
                     <tr>
                       <td>
-                        <span class="check">
-                          <input type="checkbox" name="check_list[]" alt="Checkbox" value="merah"> Dashboard</span>
+                        <span class="check"><i class=" fa fa-ellipsis-v"></i> Dashboard</span>
                       </td>
                     </tr>
 
                     <tr>
-                      <td>
-                        <span class="check"><input type="checkbox" class="checked"> Setting</span>
+                      <td><i class=" fa fa-ellipsis-v"></i> Setting
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Profil Instansi
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Ms Menu
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Ms Group
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Ms User
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">User In Group
+                          </label>
+                        </div>
                       </td>
-
                     </tr>
 
                     <tr>
-                      <td>
-                        <span class="check"><input type="checkbox" class="checked"> Master Data</span>
+                      <td><i class=" fa fa-ellipsis-v"></i> Master Data
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Kecamatan
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Desa
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data RW
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data RT
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Faskes
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Kader
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Jenis KB
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Kunjungan Ulang Akseptor
+                          </label>
+                        </div>
                       </td>
+                    </tr>
+                      <td><i class=" fa fa-ellipsis-v"></i> Transaksi
+
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data KK
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Penduduk
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Akseptor KB
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data RT
+                          </label>
+                        </div>
+                      </td>
+                    <tr>
+                      
                     </tr>
 
                     <tr>
-                      <td>
-                        <span class="check"><input type="checkbox" class="checked"> Transaksi</span>
-                      </td>
-                    </tr>
-
-                    <tr>
-                      <td>
-                        <span class="check"><input type="checkbox" class="checked"> Laporan</span>
+                      <td><i class=" fa fa-ellipsis-v"></i> Laporan
                       </td>
                     </tr>
                      
@@ -55,17 +136,161 @@
                 </table>
               </div>
             </div>
+            </div>
 
-            
-                  <button type="submit" class="btn btn-primary">Simpan</button>
-                  <a href="<?php echo base_url() . 'group'; ?>" class="btn btn-danger">Kembali</a>
+            <div class="col-lg-12">
+            <div class="form-panel">
+              <div class="custom-check goleft mt">
+                <h5><i class="fa fa-tasks"></i> User Dinas</h5>
+                <table id="todo" class="table table-hover custom-check">
+                  <tbody>
+
+                    <tr>
+                      <td>
+                        <span class="check"><i class=" fa fa-ellipsis-v"></i> Dashboard</span>
+                      </td>
+                    </tr>
+
+                      <td><i class=" fa fa-ellipsis-v"></i> Transaksi
+
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data KK
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Penduduk
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Akseptor KB
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data RT
+                          </label>
+                        </div>
+                      </td>
+                    <tr>
+                      
+                    </tr>
+
+                    <tr>
+                      <td><i class=" fa fa-ellipsis-v"></i> Laporan
+                      </td>
+                    </tr>
+                     
+                  </tbody>
+
+                </table>
               </div>
             </div>
-          </div>
-        </div>
-        </div>
+            </div>
+
+            
+            <div class="col-lg-12">
+            <div class="form-panel">
+              <div class="custom-check goleft mt">
+                <h5><i class="fa fa-tasks"></i> Kader Lapangan</h5>
+                <table id="todo" class="table table-hover custom-check">
+                  <tbody>
+
+                    <tr>
+                      <td>
+                        <span class="check"><i class=" fa fa-ellipsis-v"></i> Dashboard</span>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td><i class=" fa fa-ellipsis-v"></i> Master Data
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Kecamatan
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Desa
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data RW
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data RT
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Faskes
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Kader
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Jenis KB
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Kunjungan Ulang Akseptor
+                          </label>
+                        </div>
+                      </td>
+                    </tr>
+                      <td><i class=" fa fa-ellipsis-v"></i> Transaksi
+
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data KK
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Penduduk
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data Akseptor KB
+                          </label>
+                        </div>
+                        <div class="checkbox">
+                          <label>
+                            <input type="checkbox" value="">Data RT
+                          </label>
+                        </div>
+                      </td>
+                    <tr>
+                      
+                    </tr>
+
+                    <tr>
+                      <td><i class=" fa fa-ellipsis-v"></i> Laporan
+                      </td>
+                    </tr>
+                     
+                  </tbody>
+
+                </table>
+              </div>
+            </div>
+            </div>
+
+                  <button type="submit" class="btn btn-primary">Simpan</button>
+                  <a href="<?php echo base_url() . 'group'; ?>" class="btn btn-danger">Kembali</a>
+              
+        
       </section>
     </section>
 
-                 
-                
